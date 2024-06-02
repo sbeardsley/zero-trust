e5ad39c4944761ea21527c1f52e104817cd50850
Scott Beardsley <scott@beards.ly>
2024-06-02 10:10:44 -0400

    updating to mult-layer config and caching in docker

--- Dockerfile
+++ Dockerfile
@@ -1,15 +1,26 @@
-FROM rust:1.78 as build
-WORKDIR /build
+FROM rust:1.78 AS chef 
+# We only pay the installation cost once, 
+# it will be cached from the second build onwards
+RUN cargo install cargo-chef 
+WORKDIR /app
+
+FROM chef AS planner
 COPY . .
-RUN cargo build --release
+RUN cargo chef prepare  --recipe-path recipe.json
+
+FROM chef AS builder
+COPY --from=planner /app/recipe.json recipe.json
+# Build dependencies - this is the caching Docker layer!
+RUN cargo chef cook --release --recipe-path recipe.json
+# Build application
+COPY . .
+RUN cargo build --release --bin zero_trust_auth
 
 # our final base
 FROM debian:bookworm
 WORKDIR /app
 
-# copy the build artifact from the build stage
-COPY --from=build /build/target/release/zero_trust_auth .
-COPY --from=build /build/configuration.yaml .
-
-# set the startup command to run your binary
-CMD ["./zero_trust_auth"]
+COPY --from=builder /app/target/release/zero_trust_auth zero_trust_auth
+COPY configuration configuration
+ENV APP_ENVIRONMENT production
+ENTRYPOINT ["./zero_trust_auth"]
--- src/configuration.rs
+++ src/configuration.rs
@@ -1,11 +1,9 @@
-use config::Environment;
 use secrecy::{ExposeSecret, Secret};
 #[derive(serde::Deserialize)]
 pub struct Settings {
     pub database: DatabaseSettings,
     pub application: ApplicationSettings,
 }
-
 #[derive(serde::Deserialize)]
 pub struct ApplicationSettings {
     pub host: String,
@@ -21,12 +19,50 @@ pub struct DatabaseSettings {
     pub database_name: String,
 }
 
+pub enum Environment {
+    Local,
+    Production,
+}
+
+impl Environment {
+    pub fn as_str(&self) -> &'static str {
+        match self {
+            Environment::Local => "local",
+            Environment::Production => "production",
+        }
+    }
+}
+
+impl TryFrom<String> for Environment {
+    type Error = String;
+
+    fn try_from(value: String) -> Result<Self, Self::Error> {
+        match value.to_lowercase().as_str() {
+            "local" => Ok(Environment::Local),
+            "production" => Ok(Environment::Production),
+            _ => Err(format!("{} is not a supported environment.", value)),
+        }
+    }
+}
+
 pub fn get_configuration() -> Result<Settings, config::ConfigError> {
-    let source = Environment::default().separator("_").ignore_empty(true);
+    let source = config::Environment::default()
+        .separator("_")
+        .ignore_empty(true);
+    let base_path = std::env::current_dir().expect("Failed to determine the current directory");
+    let configuration_directory = base_path.join("configuration");
+    let environment: Environment = std::env::var("APP_ENVIRONMENT")
+        .unwrap_or_else(|_| "local".into())
+        .try_into()
+        .expect("Failed to parse APP_ENVIRONMENT");
+    let environment_filename = format!("{}.yaml", environment.as_str());
+
     let settings = config::Config::builder()
-        .add_source(config::File::new(
-            "configuration.yaml",
-            config::FileFormat::Yaml,
+        .add_source(config::File::from(
+            configuration_directory.join("base.yaml"),
+        ))
+        .add_source(config::File::from(
+            configuration_directory.join(environment_filename),
         ))
         .add_source(source)
         .build()?;
