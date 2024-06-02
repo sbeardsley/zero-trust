4a5c4bd8b2c5801dc50d474c9bac45e42c7dd5ef
Scott Beardsley <scott@beards.ly>
2024-06-01 21:36:03 -0400

    adding telemetry in chapter 4 of zero 2 prod

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,13 @@ use sqlx::PgPool;
 use std::net::TcpListener;
 use zero_trust_auth::configuration::get_configuration;
 use zero_trust_auth::startup::run;
+use zero_trust_auth::telemetry::{get_subscriber, init_subscriber};
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
+    let subscriber = get_subscriber("zero_trust_auth".into(), "info".into(), std::io::stdout);
+    init_subscriber(subscriber);
+
     let configuration = get_configuration().expect("Failed to read configuration.");
     let connection_pool = PgPool::connect(&configuration.database.connection_string())
         .await
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,8 +1,22 @@
+use once_cell::sync::Lazy;
 use sqlx::{Connection, Executor, PgConnection, PgPool};
 use std::net::TcpListener;
 use uuid::Uuid;
 use zero_trust_auth::configuration::{get_configuration, DatabaseSettings};
 use zero_trust_auth::startup::run;
+use zero_trust_auth::telemetry::{get_subscriber, init_subscriber};
+
+static TRACING: Lazy<()> = Lazy::new(|| {
+    let default_filter_level = "info".to_string();
+    let subscriber_name = "test".to_string();
+    if std::env::var("TEST_LOG").is_ok() {
+        let subscriber = get_subscriber(subscriber_name, default_filter_level, std::io::stdout);
+        init_subscriber(subscriber);
+    } else {
+        let subscriber = get_subscriber(subscriber_name, default_filter_level, std::io::sink);
+        init_subscriber(subscriber);
+    }
+});
 
 pub struct TestApp {
     pub address: String,
@@ -86,6 +100,7 @@ async fn subscribe_returns_a_400_when_data_is_missing() {
 }
 
 async fn spawn_app() -> TestApp {
+    Lazy::force(&TRACING);
     let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind address");
     let address = listener.local_addr().unwrap().to_string();
     let mut configuration = get_configuration().expect("Failed to read configuration.");
